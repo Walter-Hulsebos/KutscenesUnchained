121a17bfc0cf9b13ce06630d75c29240b833dffa
Walter Hulsebos <walterhulsebos@outlook.com>
2023-02-28 19:49:42 +0100

    ✨HighlightableTag

---
+++ Assets/03_Scripts/02_Selection/Components/HighlightableTagAuthoring.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using Unity.Entities;
+
+namespace Game.Selection
+{
+    public sealed class HighlightableTagAuthoring : MonoBehaviour
+    {
+        private sealed class HighlightableTagBaker : Baker<HighlightableTagAuthoring>
+        {
+            public override void Bake(HighlightableTagAuthoring authoring)
+            {
+                AddComponent(new HighlightableTag());
+            }
+        }
+    }
+}
