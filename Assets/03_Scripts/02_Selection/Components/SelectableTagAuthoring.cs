50652e89d4191a360e0f80831e97470cb2912c8b
Walter Hulsebos <walterhulsebos@outlook.com>
2023-02-28 19:50:07 +0100

    ✨SelectableTag

---
+++ Assets/03_Scripts/02_Selection/Components/SelectableTagAuthoring.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using Unity.Entities;
+
+namespace Game.Selection
+{
+    public sealed class SelectableTagAuthoring : MonoBehaviour
+    {
+        private sealed class SelectableTagBaker : Baker<SelectableTagAuthoring>
+        {
+            public override void Bake(SelectableTagAuthoring authoring)
+            {
+                AddComponent(new SelectableTag());
+            }
+        }
+    }
+}
